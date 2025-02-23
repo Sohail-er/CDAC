3c8bf3ceaf0a719e3f02d5a5b98caf5ef138cd10
Sohail Khan <sohail.er01@gmail.com>
2025-02-24 00:56:45 +0530

    Add files via upload

---
+++ Java Assignments/Assignmnet 1/assignment_1.java
@@ -0,0 +1,36 @@
+class positive{
+	public static void main(String args[]){
+	int num = -8;
+
+	if (num>=0){
+		System.out.println("positive");
+	}else{
+		System.out.println("negative");
+	}
+}
+}
+class Rect{
+	public static void main(String args[]){
+	int l = 8 ;
+	int w = 20;
+	int area = l * w;
+
+	System.out.println("Area of Rect:"+area);
+
+}
+}
+class GoodMorning{
+	public static void main(String args[]){
+	char period1= 'A';
+	char period2= 'M';
+	int time =9;
+	if (period1 == 'A' && period2 == 'M' && time >=5 && time < 12){
+		System.out.println("Good Morning");
+	}else{
+		System.out.println("Not morning time");
+	}
+	
+	
+
+}
+}
